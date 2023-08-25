1ed879472d0fe5e99d26e4b1246c9ff18ecb105f
mustbepositive <572434262@qq.com>
2023-08-25 18:54:56 +0800

    8-25 18:54 关浩彬 indep_kivy_file系列文件实现窗口之间的相互独立

---
+++ indep_kivy_file1.py
@@ -0,0 +1,66 @@
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.app import App
+from kivy.core.text import LabelBase
+from kivy.uix import widget
+from kivy.graphics import Color, Rectangle
+from indep_kivy_file2 import basic_video_window
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.videoplayer import VideoPlayer  ##引入控件
+# 统一中文字体
+LabelBase.register(name='Font_Hanzi', fn_regular='kvcn.ttc')  # 导入字体文件
+Window.size = (900, 700)
+from opt import opt
+import sys
+# 获取视频基础数据
+from MySql.connect_mysql import VIDEO_data2
+video_play_data = VIDEO_data2()
+
+
+video_id = video_play_data[0::3]
+video_name = video_play_data[1::3]
+
+from dict_base import *
+
+args = opt
+args.dataset = 'coco'
+q = []  # (存放30帧实时数据)
+L = []  # 存放已执行函数
+
+list_father = []
+class main_window(FloatLayout):
+    def __init__(self, **kwargs):
+        super(main_window, self).__init__(**kwargs)
+        # "录入标准数据"按钮
+        self.add_widget(Button
+                        (text="录 入 标 准 数 据",
+                         font_name='kvcn.ttc',
+                         font_size=18,
+                         background_color=[148 / 155, 242 / 155, 249 / 155],
+                         size_hint=(0.17, 0.1),
+                         pos_hint={"x": 0.1, "top": 0.8},
+                         on_press=self.basic_video_window))
+        # "选择视频"按钮
+        self.add_widget(Button(text="选 择 视 频",
+                               font_name='kvcn.ttc',
+                               font_size=18,
+                               background_color=[148 / 155, 242 / 155, 249 / 155],
+                               size_hint=(0.17, 0.1),
+                               pos_hint={"x": 0.1, "top": 0.6}))
+    # 用于前往"basic_video_window"页面
+    def basic_video_window(self, *args):
+        self.root = basic_video_window()
+
+class main_App(App):
+    def __init__(self, **kwargs):
+        super(main_App, self).__init__(**kwargs)
+        self.root = main_window()
+        self.title = "主页面"
+
+
+if __name__ == '__main__':
+    main_App().run()
---
+++ indep_kivy_file2.py
@@ -0,0 +1,60 @@
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+
+class basic_video_window(FloatLayout):
+    def __init__(self, **kwargs):
+        super(basic_video_window, self).__init__(**kwargs)
+        # “视频编号”标签和输入框
+        self.add_widget(Label(text="视 频 编 号",
+                              font_name='Font_Hanzi',
+                              size_hint=(0.045, 0.05),
+                              pos_hint={"x": 0.23, "top": 0.62}))
+        self.add_widget(TextInput(multiline=False,
+                                  font_name='Font_Hanzi',
+                                  size_hint=(0.3, 0.15),
+                                  pos_hint={"x": 0.1, "top": 0.85}))
+        # "视频名称"标签和输入框
+        self.add_widget(Label(text="视 频 名 称",
+                              font_name='Font_Hanzi',
+                              size_hint=(0.045, 0.05),
+                              pos_hint={"x": 0.73, "top": 0.62}))
+        self.add_widget(TextInput(multiline=False,
+                                  font_name='Font_Hanzi',
+                                  size_hint=(0.3, 0.15),
+                                  pos_hint={"x": 0.6, "top": 0.85}))
+        # "视频时长"标签和输入框
+        self.add_widget(Label(text="视 频 时 长",
+                              font_name='Font_Hanzi',
+                              size_hint=(0.045, 0.05),
+                              pos_hint={"x": 0.23, "top": 0.21}))
+        self.add_widget(TextInput(multiline=False,
+                                  font_name='Font_Hanzi',
+                                  size_hint=(0.3, 0.15),
+                                  pos_hint={"x": 0.1, "top": 0.45}))
+        # 窗口底部的提示标签
+        self.add_widget(Label(text="(视频存储编号自定义且不可重复)",
+                              font_name='Font_Hanzi',
+                              size_hint=(0.03, 0.04),
+                              pos_hint={"x": 0.49, "top": 0.10}))
+        # “确认”按钮
+        self.add_widget(Button(text="确 认",
+                               font_name='Font_Hanzi',
+                               background_color=(148 / 155, 242 / 155, 249 / 155),
+                               size_hint=(0.3, 0.2),
+                               pos_hint={"x": 0.6, "top": 0.45}))
+        # 显示弹窗
+        window = Popup(title="input the basic information of the video",
+                       content=self,
+                       size_hint=(None, None),
+                       size=(500, 300))
+        window.open()
+        # 右上角关闭页面按钮
+        self.add_widget(Button(text="×",
+                               font_size=21,
+                               background_color=[1, 0, 0, 1],
+                               size_hint=(0.05, 0.05),
+                               pos_hint={"x": 0.95, "top": 1},
+                               on_press=window.dismiss))
